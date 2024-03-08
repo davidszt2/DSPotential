27958d7b97b61670e3ccfcb55993d1b7ee54169f
David Moeller Sztajnbok <david.sztajnbok@gmail.com>
2024-03-08 04:13:04 -0800

    Rankine Oval + fixes

--- PotentialFlows/RankineOval.py
+++ PotentialFlows/RankineOval.py
@@ -1,5 +1,5 @@
 """
-Rankine Semi-Oval Class
+Rankine Oval Class
 
 DSPotential
 """
@@ -8,27 +8,24 @@ import numpy as np
 from PotentialFlows.SourceSink import SourceSink
 from PotentialFlows.Uniform import UniformFlow
 
-
-class RankineSemiOval():
-    def __init__(self, ovalHalfHeight, uniformStrength, uniformAngle=0, sourceX=0, sourceY=0):
+class RankineOval():
+    def __init__(self, sourceSinkStrength, sourceSinkDistance, uniformStrength, uniformAngle=0):
         """
         Initializes the Rankine Oval
         """
-        self.ovalHalfHeight = ovalHalfHeight
+        
+        self.sourceSinkStrength = sourceSinkStrength
+        self.sourceSinkDistance = sourceSinkDistance
         self.uniformStrength = uniformStrength
-        self.sourceX = sourceX
-        self.sourceY = sourceY
+        self.uniformAngle = uniformAngle
         
-        self.sourceStrength = 2 * uniformStrength * ovalHalfHeight
-        self.source = SourceSink(self.sourceStrength, sourceX, sourceY)
+        self.source = SourceSink(sourceSinkStrength, -sourceSinkDistance/2, 0)
+        self.sink = SourceSink(-sourceSinkStrength, sourceSinkDistance/2, 0)
         self.uniform = UniformFlow(uniformStrength, uniformAngle)
-        
-        self.xSP = -self.sourceStrength / (2 * np.pi * uniformStrength)
-        self.ySP = 0
         
     def velocityPotential(self, X, Y):
         """
-        Returns the velocity potential of the Rankine Semi-Oval
+        Returns the velocity potential of the Rankine Oval
 
         Parameters
         ----------
@@ -40,17 +37,18 @@ class RankineSemiOval():
         Returns
         -------
         psi: ndarray
-            Velocity potential of the Rankine Semi-Oval
+            Velocity potential of the Rankine Oval
         """
         sourcePotential = self.source.velocityPotential(X, Y)
+        sinkPotential = self.sink.velocityPotential(X, Y)
         uniformPotential = self.uniform.velocityPotential(X, Y)
         
-        psi = sourcePotential + uniformPotential
+        psi = sourcePotential + sinkPotential + uniformPotential
         return psi
     
     def streamFunction(self, X, Y):
         """
-        Returns the stream function of the Rankine Semi-Oval
+        Returns the stream function of the Rankine Oval
 
         Parameters
         ----------
@@ -62,17 +60,18 @@ class RankineSemiOval():
         Returns
         -------
         psi: ndarray
-            Stream function of the Rankine Semi-Oval
+            Stream function of the Rankine Oval
         """
         sourceStream = self.source.streamFunction(X, Y)
+        sinkStream = self.sink.streamFunction(X, Y)
         uniformStream = self.uniform.streamFunction(X, Y)
         
-        psi = sourceStream + uniformStream
+        psi = sourceStream + sinkStream + uniformStream
         return psi
     
     def velocityField(self, X, Y):
         """
-        Returns the velocity field of the Rankine Semi-Oval
+        Returns the velocity field of the Rankine Oval
 
         Parameters
         ----------
@@ -89,8 +88,11 @@ class RankineSemiOval():
             y-component of the velocity field
         """
         uSource, vSource = self.source.velocityField(X, Y)
+        uSink, vSink = self.sink.velocityField(X, Y)
         uUniform, vUniform = self.uniform.velocityField(X, Y)
         
-        u = uSource + uUniform
-        v = vSource + vUniform
+        u = uSource + uSink + uUniform
+        v = vSource + vSink + vUniform
+        
         return u, v
+    
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from MeshTools import setupGrid
 from PotentialFlows.SourceSink import SourceSink
 from PotentialFlows.Uniform import UniformFlow
 from PotentialFlows.RankineSemiOval import RankineSemiOval
+from PotentialFlows.RankineOval import RankineOval
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -23,19 +24,21 @@ source1 = SourceSink(1.0, -0.5, 0.0)
 source2 = SourceSink(1.0, -0.5, 0.0)
 sink1 = SourceSink(-1.0, 0.5, 0.0)
 uniform1 = UniformFlow(1.0, 0.0)
-semiOval1 = RankineSemiOval(1.0, 1.0)
+semiOval1 = RankineSemiOval(0.25, 1.0)
+oval1 = RankineOval(1.0, 1.0, 1.0)
 
 uSource1, vSource1 = source1.velocityField(X, Y)
 uSource2, vSource2 = source2.velocityField(X, Y)
 uSink1, vSink1 = sink1.velocityField(X, Y)
 uUniform1, vUniform1 = uniform1.velocityField(X, Y)
+uSemiOval1, vSemiOval1 = semiOval1.velocityField(X, Y)
+uOval1, vOval1 = oval1.velocityField(X, Y)
 
 """EXAMPLE 1: SOURCE FLOW"""
 plt.figure()
 plt.title("Example 1: Source")
 plt.streamplot(X, Y, uSource1, vSource1, density=2, linewidth=1, arrowsize=2, arrowstyle='->')
 plt.scatter(source1.x, source1.y, color='r', marker='o', s=80)
-# plt.show()
 
 """EXAMPLE 2: SUPERPOSITION OF TWO SOURCES"""
 plt.figure()
@@ -45,7 +48,6 @@ vTotal = vSource1 + vSource2
 plt.streamplot(X, Y, uTotal, vTotal, density=2, linewidth=1, arrowsize=2, arrowstyle='->')
 plt.scatter(source1.x, source1.y, color='r', marker='o', s=80)
 plt.scatter(source2.x, source2.y, color='g', marker='o', s=80)
-# plt.show()
 
 """EXAMPLE 3: SUPERPOSITION OF SOURCES AND SINK (DOUBLET)"""
 plt.figure()
@@ -55,21 +57,19 @@ vTotal = vSource1 + vSink1
 plt.streamplot(X, Y, uTotal, vTotal, density=2, linewidth=1, arrowsize=2, arrowstyle='->')
 plt.scatter(source1.x, source1.y, color='r', marker='o', s=80)
 plt.scatter(sink1.x, sink1.y, color='g', marker='o', s=80)
-# plt.show()
 
-"""EXAMPLE 4: RANKINE OVAL (DOUBLET + UNIFORM FLOW)"""
+"""EXAMPLE 4: RANKINE OVAL (SOURCE + SINK + UNIFORM FLOW)"""
 plt.figure()
 plt.title("Example 4: Rankine Oval")
-uTotal = uUniform1 + uSource1 + uSink1
-vTotal = vUniform1 + vSource1 + vSink1
-plt.streamplot(X, Y, uTotal, vTotal, density=2, linewidth=1, arrowsize=2, arrowstyle='->')
+plt.streamplot(X, Y, uOval1, vOval1, density=2, linewidth=1, arrowsize=2, arrowstyle='->')
 plt.scatter(source1.x, source1.y, color='r', marker='o', s=80)
 plt.scatter(sink1.x, sink1.y, color='g', marker='o', s=80)
 
 """EXAMPLE 5: RANKINE SEMI-OVAL (SOURCE + UNIFORM FLOW)"""
 plt.figure()
 plt.title("Example 5: Rankine Semi-Oval")
-uSemiOval, vSemiOval = semiOval1.velocityField(X, Y)
-plt.streamplot(X, Y, uSemiOval, vSemiOval, density=2, linewidth=1, arrowsize=2, arrowstyle='->')
-plt.scatter(semiOval1.sourceX, semiOval1.sourceY, color='r', marker='o', s=80)
+plt.streamplot(X, Y, uSemiOval1, vSemiOval1, density=2, linewidth=1, arrowsize=2, arrowstyle='->')
+plt.scatter(semiOval1.sourceX, semiOval1.sourceY, color='r', marker='o', s=40)
+plt.scatter(semiOval1.xSP, semiOval1.ySP, color='g', marker='o', s=40)
+plt.contour(X, Y, semiOval1.streamFunction(X, Y), levels=[-semiOval1.sourceStrength/2, semiOval1.sourceStrength/2], colors='r', linestyles='solid')
 plt.show()
